1db9fbbd4f504884926d9e609948595fb455b766
dimimall <malliaroy@gmail.com>
2019-05-23 10:54:49 +0300

    add note activity

--- app/src/main/java/com/example/dailyreminder/activities/NoteActivity.java
+++ app/src/main/java/com/example/dailyreminder/activities/NoteActivity.java
@@ -1,13 +1,15 @@
-package com.example.dailyreminder;
+package com.example.dailyreminder.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dailyreminder.R;
+
+public class NoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note);
     }
 }
